18313a68b0bb4c3d2cf1552d5e1f26366ebf09db
Elar Huik <40868085+elarhuik@users.noreply.github.com>
2018-10-02 23:05:58 +0300

    update api endpoint config
    using request library to read data

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,14 @@
 export const API_KEY = 'insert your unique api key here';
 
-export const API_PATH = '';
+const API_PATH = `https://api.pipedrive.com/v1/persons`;
+
+export const persons = {
+  uri: API_PATH,
+  qs: {
+      api_token: API_KEY,
+  },
+  headers: {
+      'User-Agent': 'Request-Promise'
+  },
+  json: true,
+};
